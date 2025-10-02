389d17e0df08c314325316a98074b8f4ca0a6464
Git Hunu <hunugit30@gmail.com>
2025-10-02 15:59:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6353702608-6ab29",
+  "appId": "1:299672689419:web:153cfdf02ebf80e45b200e",
+  "apiKey": "AIzaSyCjIqWBifoRy5pflUlwrIteqcbiOauJ0mM",
+  "authDomain": "studio-6353702608-6ab29.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "299672689419"
+};
