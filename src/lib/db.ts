a8b60a7d598bde7a158b4cc26d5b554ce75f7fab
Git Hunu <hunugit30@gmail.com>
2025-10-03 21:42:19 +0000

    Use this as the database instead postgresql://sts_eeb_user:HbdN3y53vuQ37

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,14 @@
 import { Pool } from 'pg';
-import type { Notification, CourseMaterial, Assignment, Subject } from './types';
+import type { Notification, CourseMaterial, Assignment, Subject, FileType } from './types';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export async function getNotifications(): Promise<Notification[]> {
