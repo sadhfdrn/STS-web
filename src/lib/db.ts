87715aa6b1e8d82ca4d9a37998dfaaa4a959fc82
Git Hunu <hunugit30@gmail.com>
2025-10-04 01:04:59 +0000

    Return the db back to the neon tech db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,9 +6,6 @@ dotenv.config();
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
 });
 
 export async function getNotifications(): Promise<Notification[]> {
